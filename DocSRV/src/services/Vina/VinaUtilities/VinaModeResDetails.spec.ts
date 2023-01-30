dbc9083eb4c1aeada3f1da3b3f1ff5df69cf450e
Darken <affesachraf70@gmail.com>
2023-01-30 20:35:09 +0100

    VinaModeResDetails unit tests #13

--- DocSRV/src/services/Vina/VinaUtilities/VinaModeResDetails.spec.ts
+++ DocSRV/src/services/Vina/VinaUtilities/VinaModeResDetails.spec.ts
@@ -1,11 +1,31 @@
-describe('VinaModeResDetails', () => {
+import { VinaModeResDetails } from "./VinaModeResDetails";
+
+describe('vinaModeResDetails', () => {
 
     beforeEach(() => {
     });
+    test('should parse line and init the values correctly', () => {
+        let line = '   1         -7.7      0.000      0.000';
+        let vinaModeResDetails = new VinaModeResDetails();
+        vinaModeResDetails.parseLineAndInit(line);
+        expect(vinaModeResDetails.mode).toBe(1);
+        expect(vinaModeResDetails.affinity).toBe(-7.7);
+        expect(vinaModeResDetails.rsmd_lb).toBe(0);
+        expect(vinaModeResDetails.rsmd_ub).toBe(0);
+    });
 
-    //todo
-    test('TODO Some VinaModeResDetails test here', () => {
-        expect(true).toBe(true);
+    test('should set correct values (fromJSON)', () => {
+        let json = {
+            mode: 0,
+            affinity: 1,
+            rsmd_lb: 2,
+            rsmd_ub: 3,
+        };
+        let vinaModeResDetails = VinaModeResDetails.fromJSON(json);
+        expect(vinaModeResDetails.mode).toBe(json.mode);
+        expect(vinaModeResDetails.affinity).toBe(json.affinity);
+        expect(vinaModeResDetails.rsmd_lb).toBe(json.rsmd_lb);
+        expect(vinaModeResDetails.rsmd_ub).toBe(json.rsmd_ub);
     });
 
 });
