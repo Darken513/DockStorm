813e2190754d4425983559bb4763f57ecd8ebe55
Darken <affesachraf70@gmail.com>
2023-01-30 21:40:50 +0100

    different sites positions confs #28

--- DocSRV/src/services/Vina/VinaUtilities/VinaConf.ts
+++ DocSRV/src/services/Vina/VinaUtilities/VinaConf.ts
@@ -20,10 +20,11 @@ class VinaConf {
         out: '',
         log: ''
     }
-    activeSite: string = 'AS0';
+    activeSite: string = 'AS1';
     scheduleTime: number | undefined;
     resolveTime: number | undefined;
     repeatNtimes: number = 1;
+    
     /**
     * @constructor - The constructor accepts an optional confPath parameter, and initializes the configuration from the file if provided.
     * @param {string} rawConf - The raw content of a vina configuration file.
@@ -38,6 +39,7 @@ class VinaConf {
             this.initFromPath(options.confPath);
         }
     }
+
     static fromJSON(json: any) {
         let toret = new VinaConf();
         toret.confParsed = ParsedVinaConf.fromJSON(json.confParsed);
@@ -49,54 +51,43 @@ class VinaConf {
         toret.repeatNtimes = json.repeatNtimes;
         return toret;
     }
-    /**
-    * setLigandPath - A method that sets the ligand for the AutoDock Vina configuration and re-affects
-    * the parameter out ( output file ) if it has a receptor path as well
-    * @param {string} ligand - The ligand file path 
-    * @returns {this} - The current instance of the class
-    */
+
     setLigandPath(ligand: string) {
         this.confParsed.ligand = ligand;
         return this;
     }
     //TODO Find a way to use these in a proper way 
 
-    /**
-    * setReceptorPath - A method that sets the receptor for the AutoDock Vina configuration and re-affects 
-    * the parameter out ( output file ) if it has a ligand path as well
-    * @param {string} receptor - The receptor file path 
-    * @returns {this} - The current instance of the class
-    */
     setReceptorPath(receptor: string) {
         this.confParsed.receptor = receptor;
         return this;
     }
+
     setActiveSite(name: string, vec3D: any) {
         this.activeSite = name;
         this.confParsed.center_x = vec3D.x;
         this.confParsed.center_y = vec3D.y;
         this.confParsed.center_z = vec3D.z;
     }
+
     /**
-    * A method that re-affects the output file based on the ligand and receptor paths
-    * if predifined then it decides where to make copies of them
-    * @param {string} receptor - The receptor file path 
-    * @returns {this} - The current instance of the class
+    * reAffectOutput method updates the output file path and log file path for the given input parameters.
+    * @param {number} repLef - The remaining repeat times for the process.
+    * @returns {void}
     */
     reAffectOutput(repLef: number) {
         if (!this.confParsed.receptor || !this.confParsed.ligand)
             return;
         let receptorBase = path.parse(this.confParsed.receptor).name;
         let ligandBase = path.parse(this.confParsed.ligand).name;
-        let out = path.join(
+        let basePath = path.join(
             GodService.globalConf.output.path,
             GodService.globalConf.output.folderName,
-            receptorBase.concat('_', ligandBase),
-            this.activeSite,
-            this.scheduleTime!.toString(),
-            'Try_n' + (1 + this.repeatNtimes - repLef).toString(),
-            'Result.pdbqt'
+            receptorBase.concat('_', ligandBase)
         );
+        let compositionConf = this.getCompositionConf(basePath);
+        let out = this.getOutPutPath(basePath, repLef, compositionConf);
+        this.updateCompositionConf(basePath);
         if (!this.confPath) {
             this.confParsed.out = out
             this.confParsed.log = path.join(path.parse(out).dir, 'log.txt')
@@ -121,6 +112,7 @@ class VinaConf {
         this.outputCopiesPath.log = path.join(path.parse(out).dir, 'log.txt')
         this.outputCopiesPath.out = out;
     }
+
     /**
     * Initializes the VinaInstance by checking if the confPath exists and parsing it
     * @param {string} confPath - The configuration file path.
@@ -159,6 +151,77 @@ class VinaConf {
             const [key, value] = line.split(" = ");
             (<any>this.confParsed)[key] = value;
         });
+    }
+
+    /**
+    * Get the composition configuration
+    * @param basePath the base path to look for the configuration
+    * @returns configuration object or `undefined` if the file is not found
+    */
+    getCompositionConf(basePath: string) {
+        let confPath = path.join(basePath, 'composition_conf.json')
+        let exists = fs.existsSync(confPath);
+        if (!exists)
+            return undefined;
+        return JSON.parse(fs.readFileSync(confPath).toString());
+    }
+
+    /**
+    * Get the output path
+    * @param basePath the base path to construct the output path
+    * @param repLeft the number of repetitions left
+    * @param compositionConf the composition configuration object
+    * @returns the constructed output path
+    */
+    getOutPutPath(basePath: string, repLeft: number, compositionConf: any) {
+        let site = compositionConf ? compositionConf.sites.find((site: any) => {
+            return parseFloat(site.center_x) == this.confParsed.center_x &&
+                parseFloat(site.center_y) == this.confParsed.center_y &&
+                parseFloat(site.center_z) == this.confParsed.center_z
+        }) : undefined;
+        this.activeSite = site
+            ? site.label
+            : compositionConf && compositionConf.sites
+                ? 'AS' + compositionConf.sites.length + 1
+                : 'AS1';
+        return path.join(
+            basePath,
+            this.activeSite,
+            this.scheduleTime!.toString(),
+            'Try_n' + (1 + this.repeatNtimes - repLeft).toString(),
+            'Result.pdbqt'
+        );
+    }
+
+    /**
+    * Update the composition configuration
+    * @param basePath the base path to look for the configuration
+    */
+    updateCompositionConf(basePath: string) {
+        let confPath = path.join(basePath, 'composition_conf.json')
+        let exists = fs.existsSync(confPath);
+        let siteData = {
+            label: this.activeSite,
+            center_x: this.confParsed.center_x,
+            center_y: this.confParsed.center_y,
+            center_z: this.confParsed.center_z,
+        }
+        if (exists) {
+            let compositionConf = JSON.parse(fs.readFileSync(confPath).toString());
+            let site = compositionConf ? compositionConf.sites.find((site: any) => {
+                return parseFloat(site.center_x) == this.confParsed.center_x &&
+                    parseFloat(site.center_y) == this.confParsed.center_y &&
+                    parseFloat(site.center_z) == this.confParsed.center_z
+            }) : undefined;
+            if (site)
+                return;
+            compositionConf.sites.push(siteData)
+            fs.writeFileSync(confPath, JSON.stringify(compositionConf))
+        } else {
+            fs.writeFileSync(confPath, JSON.stringify({
+                sites: [siteData]
+            }))
+        }
     }
 }
 
